5a8722ac81156f52fcf9a6fe74d7c890af826adf
Daria Terenteva <d.terenteva@infomaximum.biz>
2025-04-14 10:23:26 +0600

    Added image`s credit

--- src/App.tsx
+++ src/App.tsx
@@ -15,28 +15,39 @@ export const App: React.FC = () => {
   }, [targetDate]);
 
   return (
-    <div className="wrapper">
-      <div className="date-counter">
-        <h1>До отпуска осталось</h1>
-        <h2 className="date-items">
-          <div className="date-item">
-            <div className="date-count">{timeLeft.days}</div>
-            <div className="date-label">дня</div>
-          </div>
-          <div className="date-item">
-            <div className="date-count">{timeLeft.hours}</div>
-            <div className="date-label">часов</div>
-          </div>
-          <div className="date-item">
-            <div className="date-count">{timeLeft.minutes}</div>
-            <div className="date-label">минут</div>
-          </div>
-          <div className="date-item">
-            <div className="date-count">{timeLeft.seconds}</div>
-            <div className="date-label">секунд</div>
-          </div>
-        </h2>
+    <>
+      <div className="wrapper">
+        <div className="date-counter">
+          <h1>До отпуска осталось</h1>
+          <h2 className="date-items">
+            <div className="date-item">
+              <div className="date-count">{timeLeft.days}</div>
+              <div className="date-label">дня</div>
+            </div>
+            <div className="date-item">
+              <div className="date-count">{timeLeft.hours}</div>
+              <div className="date-label">часов</div>
+            </div>
+            <div className="date-item">
+              <div className="date-count">{timeLeft.minutes}</div>
+              <div className="date-label">минут</div>
+            </div>
+            <div className="date-item">
+              <div className="date-count">{timeLeft.seconds}</div>
+              <div className="date-label">секунд</div>
+            </div>
+          </h2>
+        </div>
       </div>
-    </div>
+      <div className="footer">
+        <a
+          href="https://unsplash.com/photos/yellow-flower-field-near-snow-covered-mountain-during-daytime-K9cCqYhmodk"
+          target="_blank"
+          rel="noreferrer"
+        >
+          image
+        </a>
+      </div>
+    </>
   );
 };
